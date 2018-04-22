584e867d49a8325f9c63f9d3f35931341ccd0f07
David Daly <david-daly@live.ie>
2018-04-22 18:53:38 +0100

    Build initial structure for app

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-toolbar dark color="green">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title class="white--text">{{title}}</v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
