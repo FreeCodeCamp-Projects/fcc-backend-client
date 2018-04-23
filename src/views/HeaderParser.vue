76d8559434f810c82495aba52f1ce2ffe0b0bf0e
David Daly <david-daly@live.ie>
2018-04-23 22:18:58 +0100

    Add initial views for each of the challenges

---
+++ src/views/HeaderParser.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout>
+    <v-flex>
+        <div>
+            <h1>Header Parser Service</h1>
+        </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'headerParser',
+  components: {
+  }
+}
+</script>
---
+++ src/views/ShortenUrl.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout>
+    <v-flex>
+        <div>
+            <h1>URL Shortener Service</h1>
+        </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'urlShortener',
+  components: {
+  }
+}
+</script>
